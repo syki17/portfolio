0a341ca0277a7b7099804a976bb6a548945a1413
Jakub <jakub.syk@gmail.com>
2019-02-24 23:52:52 -0500

    updated routes and views

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,49 @@
+/*
+On render, get info about my repo via API call using node-fetch, fill the arrays,
+and send to front end. 
+*/
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+const fetch = require('node-fetch');
+
+// initialize empty arrays
+var links = [];
+var desc = [];
+var title = [];
 
 /* GET Projects page. */
 router.get('/', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+
+  //API call to get my repo (rate limited to 60 per hour)
+  fetch('https://api.github.com/users/syki17/repos')
+  .then(response => response.json())
+    .then(body => {
+      for (var i=0;i < body.length;i++){
+        
+        //populate arrays
+        links.push(body[i].html_url)
+        desc.push(body[i].description)
+        title.push(body[i].name);
+        
+
+      }
+      res.render('projects', {
+        link: links,
+        description: desc,
+        titles: title
+      });
+      console.log(links);
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+
+  
+// clear arrays, so it doesn't keep adding to the same array.
+  links =[];
+  desc =[];
+  title =[];
 });
 
 module.exports = router;
--- routes/services.js
+++ routes/services.js
@@ -1,9 +1,40 @@
+/*
+Code to render the services page
+on response it returns a JSON with info about frameworks
+*/
 var express = require('express');
 var router = express.Router();
 
 /* GET Services page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', 
+  { title: 'Services',
+    java: [{
+      title:'Jackson',
+      link:'https://github.com/FasterXML/jackson',
+      desc: 'Jackson is a suite of data-processing tools for Java (and the JVM platform)'
+    },
+    {
+      title:'Gson',
+      link:'https://github.com/google/gson',
+      desc:'Gson is a Java library that can be used to convert Java Objects into their JSON representation'
+    },
+    {
+      title:'OpenCSV',
+      link: 'http://opencsv.sourceforge.net/',
+      desc:'Opencsv is an easy-to-use CSV (comma-separated values) parser library for Java'
+    }],
+    cSharp: [{
+      title: '.Net',
+      link :'https://docs.microsoft.com/en-us/dotnet/welcome',
+      desc: 'NET is a developer platform with tools and libraries for building any type of app, including web, mobile, desktop, gaming, IoT, cloud, and microservices.'
+    },
+    {
+      title: 'ASP.Net',
+      link :'https://dotnet.microsoft.com/apps/aspnet',
+      desc: 'ASP.NET is an open-source server-side web application framework designed for web development to produce dynamic web pages.'
+    }]
+  });
 });
 
 module.exports = router;
